43d32e1c3449c2be5f9bcd6614f938aab4c48a15
Mike Chirico (https://mchirico.github.io/) <mchirico@gmail.com>
2020-11-22 23:03:43 -0500

    update-automated-save

---
+++ examples/putGet/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"github.com/mchirico/go.etcd/pkg/etcdutils"
+	"log"
+	"time"
+)
+
+func main() {
+
+	e, cancel := etcdutils.NewETC()
+	defer cancel()
+
+	e.DeleteWithPrefix("/testing")
+
+	now := time.Now()
+
+	e.Put("/testing/TestETC_Put ... more", now.String())
+	e.PutWithLease("/testing/a", now.String(), 3)
+
+	result, _ := e.GetWithPrefix("/testing/")
+
+	if len(result.Kvs) != 2 {
+		log.Fatalf("Number of keys: %d\n", len(result.Kvs))
+	}
+
+	for i, v := range result.Kvs {
+		log.Printf("result.Kvs[%d]: %s, ver: %d,  lease: %d\n", i, v.Value, v.Version, v.Lease)
+	}
+
+}

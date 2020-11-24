1e9a77f30994dca7ebe1748cf1568e0e9eaaa100
Mike Chirico (https://mchirico.github.io/) <mchirico@gmail.com>
2020-11-24 17:38:58 -0500

    update-automated-save

--- examples/putTimer/main.go
+++ examples/putTimer/main.go
@@ -11,12 +11,10 @@ func Update() {
 	e, cancel := etcdutils.NewETC()
 	defer cancel()
 
-
 	now := time.Now()
 
 	e.PutWithLease("gopi.service/update", now.String(), 120)
 	e.PutWithLease("gopi.service/addr", Ifconfig(), 120)
-
 
 	result, _ := e.GetWithPrefix("gopi.service")
 
@@ -24,11 +22,13 @@ func Update() {
 		log.Printf("result.Kvs[%d]: %s, ver: %d,  lease: %d\n", i, v.Value, v.Version, v.Lease)
 	}
 
-
 }
-
 
 func main() {
 
+	for {
+		Update()
+		time.Sleep(70 * time.Second)
+	}
 
 }
